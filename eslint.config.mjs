ec129212767dc8ae0582b8e41126e6049dab8b69
PhilTBatt <philtbatt@gmail.com>
2024-11-19 11:40:51 +0000

    Formatted

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,40 @@
-import react from "eslint-plugin-react";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import react from 'eslint-plugin-react';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
 });
 
-export default [...compat.extends("plugin:react/recommended", "airbnb", "prettier"), {
-    plugins: {
-        react,
-    },
+export default [
+	...compat.extends('plugin:react/recommended', 'airbnb', 'prettier'),
+	{
+		plugins: {
+			react,
+		},
 
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
 
-        ecmaVersion: "latest",
-        sourceType: "module",
+			ecmaVersion: 'latest',
+			sourceType: 'module',
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
-        },
-    },
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
 
-    rules: {},
-}];
+		rules: {},
+	},
+];
