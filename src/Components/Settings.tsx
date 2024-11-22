4f6c9d87479ca1043493d1d806b579ab978bfb83
stevelw <steve@stevelw.co.uk>
2024-11-22 10:53:10 +0000

    add privacy form components to settings page

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Card from './Styling/Card';
 
 export default function Settings() {
 	return (
 		<>
-			<p>Settings page</p>
+			<h1>Settings</h1>
+			<Card heading="Privacy">
+				<label>
+					Visibility
+					<select>
+						<option>Public</option>
+						<option>Friends only</option>
+						<option>Hidden</option>
+					</select>
+				</label>
+			</Card>
 		</>
 	);
 }
