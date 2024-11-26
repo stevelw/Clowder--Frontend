bee225620277f5abeeb6bcbf9adeba61f2aaba93
Maria Mizamidou <m.mizamidou@hotmail.com>
2024-11-26 12:22:00 +0000

    Changes have been made

--- src/Components/CatProfile.tsx
+++ src/Components/CatProfile.tsx
@@ -6,27 +6,32 @@ import FormElement from './Styling/FormElement';
 import Button from './Styling/Button';
 import { Cat } from '../api';
 
+const loggedInUser = 'loggedInUser';
+
 function CatProfile() {
 	const [catName, setCatName] = useState<string>('');
 	const [catPicture, setCatPicture] = useState<string>('');
 	const [catDescription, setCatDescription] = useState<string>('');
 	const [catProfiles, setCatProfiles] = useState<Cat[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [message, setMessage] = useState<string>('');
 
 	useEffect(() => {
-		getCatsProfiles('USER1').then((fetchedProfiles) => {
+		getCatsProfiles('loggedInUser').then((fetchedProfiles) => {
 			if (Array.isArray(fetchedProfiles) && fetchedProfiles.length > 0) {
 				const profile = fetchedProfiles[0];
-				console.log(profile);
-
 				setCatProfiles(fetchedProfiles);
-				setIsLoading(false);
 				setCatName(profile.name);
 				setCatPicture(profile.pictureurl);
 				setCatDescription(profile.description);
 			}
+			setIsLoading(false);
 		});
 	}, []);
+
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
 
 	const handleProfileEditBy = (event: ChangeEvent<HTMLInputElement>) => {
 		//Handle editing process of Cat's profile, allow users to edit the profile
@@ -49,7 +54,7 @@ function CatProfile() {
 			description: catDescription,
 			device_id: catProfiles[0].device_id,
 			owner_id: catProfiles[0].owner_id,
-			updated_at: catProfiles[0].updated_at,
+			updated_at: new Date().toISOString(),
 			created_at: catProfiles[0].created_at,
 			deleted_at: null,
 		};
@@ -60,22 +65,23 @@ function CatProfile() {
 						profile.id === updatedProfile.id ? updatedProfile : profile
 					)
 				);
+				setMessage('Updates have been saved');
 			})
 			.catch(() => {
-				console.log('Updates havent been saved');
+				setMessage('Updates havent been saved');
 			});
 	};
 
-	const handleDeleteProfile = (event: React.MouseEvent<HTMLButtonElement>) => {
-		deleteCatProfile('USER1')
+	const handleDeleteProfile = () => {
+		deleteCatProfile('loggedInUser')
 			.then(() => {
-				console.log('Profile Removed');
 				setCatProfiles((fetchedProfiles) =>
 					fetchedProfiles.filter((profile) => profile.id !== profile.id)
 				);
+				setMessage('Profile Removed');
 			})
 			.catch(() => {
-				console.log('Failed to delete the profile');
+				setMessage('Failed to delete the profile');
 			});
 	};
 
@@ -84,6 +90,17 @@ function CatProfile() {
 			<h2 className="text-2xl font-bold text-red-950	mb-20">
 				Cat&apos;s Profile
 			</h2>
+			{message && (
+				<div
+					className={`text-2xl font-bold  mb-20
+					${message === 'Updates have been saved' ? 'text-red-950' : ''}
+					${message === 'Updates havent been saved' ? 'text-red-950' : ''}
+					${message === 'Profile Removed' ? 'text-red-950' : ''}
+					${message === 'Failed to delete the profile' ? 'text-red-950' : ''}`}
+				>
+					{message}
+				</div>
+			)}
 			<FormElement>
 				<label>
 					Name
@@ -118,7 +135,6 @@ function CatProfile() {
 				</label>
 			</FormElement>
 			<Button onClick={handleSavedChanges}>Save</Button>
-
 			<Button onClick={handleDeleteProfile}>Delete</Button>
 		</div>
 	);
