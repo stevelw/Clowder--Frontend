3389ee46aa71de74f12e0243d4a16745d3838ce1
stevelw <steve@stevelw.co.uk>
2024-11-26 16:21:23 +0000

    hf: stop mkap being covered to allow interaction

--- src/Components/Styling/OverlayWrapper.tsx
+++ src/Components/Styling/OverlayWrapper.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function OverlayWrapper({ children }: Props) {
 	return (
-		<div className="fixed mt-10 z-10 w-screen flex flex-col justify-between bottom-0 top-0">
+		<div className="fixed mt-10 z-10 w-screen flex flex-col justify-between">
 			{children}
 		</div>
 	);
