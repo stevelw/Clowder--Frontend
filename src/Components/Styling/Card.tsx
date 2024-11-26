2c048b5c33725a35888302ea736b499750905ade
stevelw <steve@stevelw.co.uk>
2024-11-26 18:34:25 +0000

    Style card

--- src/Components/Styling/Card.tsx
+++ src/Components/Styling/Card.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react';
+import H3 from './H3';
 
 interface Props {
 	children?: ReactNode;
@@ -7,8 +8,10 @@ interface Props {
 
 export default function Card(props: Props) {
 	return (
-		<div className="border-solid border-2 border-gray-200">
-			<h2>{props.heading}</h2>
+		<div className="bg-gray-200 p-5">
+			<div className="border-b-2 border-blue-500 mb-5">
+				<H3>{props.heading}</H3>
+			</div>
 			{props.children}
 		</div>
 	);
