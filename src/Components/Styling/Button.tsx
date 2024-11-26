0af9294c06e226be62375e87ec0a19045315c318
stevelw <steve@stevelw.co.uk>
2024-11-26 17:55:42 +0000

    style button

--- src/Components/Styling/Button.tsx
+++ src/Components/Styling/Button.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Button({ children, onClick }: Props) {
 	return (
 		<button
-			className="mt-16 py-10 px-20 bg-blue-500 text-white text-base border-none rounded cursor-pointer"
+			className="bg-blue-500 text-white text-base border-none rounded-lg cursor-pointer w-20 h-10 m-auto my-5"
 			onClick={onClick}
 		>
 			{children}
