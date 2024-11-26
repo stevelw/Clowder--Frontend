d3a69341e5060e1cecea629a4969767b944ef5e7
stevelw <steve@stevelw.co.uk>
2024-11-26 20:27:14 +0000

    Revert "User context"
    This reverts commit 019d0b71ea412acb67e12d3ca6d97d5fc8b3875e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import MapContainer from './Components/MapContainer';
 import Nav from './Components/Nav';
 import Settings from './Components/Settings';
-import Login from './Components/Login';
-import { UserContext } from './Contexts/UserContext';
 
 function App() {
 	const [page, setPage] = useState<String>('map');
-	const { username } = useContext(UserContext);
-
 	return (
 		<div className="h-screen">
 			<Nav setPage={setPage} />
-			{!username ? (
-				<Login />
-			) : (
-				<>
-					{page === 'map' && <MapContainer />}
-					{page === 'myClowed' && <p>My Clowed</p>}
-					{page === 'leaderboard' && <p>Leaderboard</p>}
-					{page === 'chat' && <p>Chat</p>}
-					{page === 'settings' && <Settings />}
-				</>
-			)}
+			{page === 'map' && <MapContainer />}
+			{page === 'myClowed' && <p>My Clowed</p>}
+			{page === 'leaderboard' && <p>Leaderboard</p>}
+			{page === 'chat' && <p>Chat</p>}
+			{page === 'settings' && <Settings />}
 		</div>
 	);
 }
