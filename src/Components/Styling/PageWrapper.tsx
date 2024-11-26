c7e0ece22e97f027c4050c7dd6e34636c3444e04
stevelw <steve@stevelw.co.uk>
2024-11-26 16:29:27 +0000

    style page

--- src/Components/Styling/PageWrapper.tsx
+++ src/Components/Styling/PageWrapper.tsx
@@ -6,5 +6,9 @@ interface Props {
 }
 
 export default function PageWrapper({ children }: Props) {
-	return <div className="bg-gray-50 flex-auto">{children}</div>;
+	return (
+		<div className="bg-gray-50 flex-auto rounded-t-3xl shadow-inner p-5">
+			{children}
+		</div>
+	);
 }
