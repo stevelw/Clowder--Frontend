06ced08299a49145903026fe34932b24d9163da9
stevelw <steve@stevelw.co.uk>
2024-11-27 10:59:08 +0000

    hf: remove unused imports

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,12 +1,6 @@
 import React, { SetStateAction } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faHouse,
-	faCat,
-	faTableList,
-	faComments,
-	faGear,
-} from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faCat, faGear } from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
 	setPage: React.Dispatch<SetStateAction<string>>;
