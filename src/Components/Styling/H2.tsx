2e8c4500e9fbeb76032a8b2b0d3bcb877296da45
stevelw <steve@stevelw.co.uk>
2024-11-28 08:33:39 +0000

    update h2 styling

--- src/Components/Styling/H2.tsx
+++ src/Components/Styling/H2.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export default function H2({ children }: Props) {
-	return <h2 className="text-2xl mb-5">{children}</h2>;
+	return <h2 className="text-2xl m-5">{children}</h2>;
 }
